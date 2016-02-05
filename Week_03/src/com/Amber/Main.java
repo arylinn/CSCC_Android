4f1343fadf3015a8fae094029737e8326920132e
Amber <arylinn93@gmail.com>
2016-02-04 20:37:23 -0500

    Week 3 exercise

--- Week_03/src/com/Amber/Main.java
+++ Week_03/src/com/Amber/Main.java
@@ -1,22 +1,41 @@
 package com.Amber;
 
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+
 /**
  * Created by Amber on 2/4/2016.
+ * VCS -> GIT -> COMMITFILE -> CHOOSE FILE
  */
+
 public class Main {
-    static void tempConversion(double Celsius) {
-        final String degree  = "\u00b0";
-        double F = 9.0/5 * Celsius + 32;
-        System.out.println(Celsius + degree + " C is " + F + degree + " F.");
+    static double UserInput() {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("Enter a Fahrenheit temperature between 0 and -460 ");
+        String input = scanner.nextLine();
+        return Double.parseDouble(input);
 
     }
 
+    static double Convert(double FTemp) {
+        return 5.0 / 9 * (FTemp - 32);
+    }
+
+    static void Display(double CTemp) {
+        NumberFormat format = NumberFormat.getInstance();
+        format.setRoundingMode(RoundingMode.UP);
+        format.setMaximumFractionDigits(2);
+        final String degree = "\u00b0";
+        System.out.println("The Celsius Temperature is " + format.format(CTemp) + degree);
+    }
 
     public static void main(String[] args) {
-        double CelsiusTemp = 37;
-        tempConversion(CelsiusTemp);
-
-
+        double FTemp = UserInput();
+        while (FTemp >= -460) {
+            double CTemp = Convert(FTemp);
+            Display(CTemp);
+            FTemp = UserInput();
+        }
     }
 }
 
